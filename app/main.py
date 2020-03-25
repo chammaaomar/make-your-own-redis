43d55d51a2ef65323fe2eb4a757261306889a204
Omar Chammaa <chammaa.omar@gmail.com>
2020-03-25 14:08:54 -0400

    attempted using socket low-level api; my python < 3.8

--- app/main.py
+++ app/main.py
@@ -3,9 +3,17 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    # import socket
-    # s = socket.create_server(("localhost", 6379), reuse_port=True)
-    # s.accept() # wait for client
+    import socket
+    # localhost (loopback interface; processes on host talk only)
+    HOST = "127.0.0.1"
+    PORT = 6379  # default redis port; unprivileged > 1023
+    # AF === address family; chose IPv4. SOCKET_STREAM speicifies TCP
+    # reliable (detect and resend lost packets); data arrives in-order of send
+    # there is UDP: socket.SOCK_DGRAM
+    with socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as sock:
+        # option name: reuse_addr, value = 1
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((HOST, PORT))
 
 
 if __name__ == "__main__":
